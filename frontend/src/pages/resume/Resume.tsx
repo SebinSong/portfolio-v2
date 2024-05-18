86ca49ae8e3557216f1b5918573536e94dd9da98
SebinSong <songxevin@gmail.com>
2024-05-18 21:37:51 +1200

    Add new icons to the project

--- frontend/src/pages/resume/Resume.tsx
+++ frontend/src/pages/resume/Resume.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from "react"
+import { useState, useMemo, useCallback, ReactNode } from "react"
 // child components
 import PageTemplate from "../PageTemplate"
 import TabMenu from "~/components/tab-menu/TabMenu"
@@ -11,6 +11,42 @@ import { classNames as cn } from '~/view-utils'
 // helpers
 const initTabId: ResumeEntry['id'] = resumeData[0].id
 const getPeriodDisplay = (entry: ResumeEntry): string => `${entry.period.from} - ${entry.period.to || 'Present'}`
+const getDescContent = (descItem: any, itemIndex: any): ReactNode => {
+  const isItemString = typeof descItem === 'string'
+  if (isItemString) {
+    return (
+      <div className='resume-desc-item' key={itemIndex}>
+        <i className='icon-angle-double-right item-arrow'></i>
+        <span className='item-value'>{descItem}</span>
+      </div>
+    )
+  } else if (Array.isArray(descItem)) {
+    return (
+      <div className='resume-desc-item' key={itemIndex}>
+        <ul className='item-value resume-desc-sub-list'>
+          { 
+            descItem.map((subItem, index) => (
+              <li className='sub-list-item' key={index}>
+                <i className='icon-arrow-circle-right item-arrow'></i>
+                <span>{subItem}</span>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  } else if (descItem?.link) {
+    return (
+      <div className='resume-desc-item' key={itemIndex}>
+        <i className='icon-angle-double-right item-arrow'></i>
+        <span className='item-value'>
+          {descItem.text}
+          <a className='is-link' target='_blank' href={descItem.link}>link</a>
+        </span>
+      </div>
+    )
+  }
+}
 
 import './Resume.scss'
 
@@ -45,40 +81,37 @@ export default function Resume () {
           {
             content
               ? <>
-                  <div className='resume-content-line'>
-                    <span className='resume-content-label'>period</span>
-                    <p className='resume-content-value'>{getPeriodDisplay(content)}</p>
+                  <div className='resume-content-line is-align-center mb-10'>
+                    <i className='icon-location-pin map-icon'></i>
+                    <span className='resume-location'>{content.location}</span>
                   </div>
-                  
-                  <div className='resume-content-line'>
-                    <span className='resume-content-label'>company location</span>
-                    <p className='resume-content-value'>{content.location}</p>
+
+                  <div className='period-and-work-type'>
+                    <span className='resume-period-value'>{getPeriodDisplay(content)}</span>
+                    <span className='resume-work-type'>{`${content.contractType} , ${content.workType}`}</span>
                   </div>
 
                   <div className='resume-content-line'>
-                    <span className='resume-content-label'>Roles</span>
-                    <p className='resume-content-value emphasis'>{content.roles.join(' / ')}</p>
+                    <span className='resume-content-label'>Role</span>
+                    <p className='resume-content-value'>{content.roles.join(', ')}</p>
                   </div>
 
-                  <div className='resume-content-line'>
-                    <span className='resume-content-label'>Contract type</span>
-                    <p className='resume-content-value'>
-                      <span>{`${content.contractType},`}</span>
-                      <span className='ml-4 is-inline-block emphasis'>{content.workType}</span>
-                    </p>
-                  </div>
+                  <div className='resume-content-line is-column'>
+                    <span className='resume-content-label'>Things I've done</span>
 
-                  <div className='resume-content-line'>
-                    <span className='resume-content-label'>Tech stacks</span>
-                    <div className='stacks-container'>
+                    <div className='resume-desc-container mt-10'>
                       {
-                        content.stacks.map((stack, index) => (<span className={cn('stack-tag', index % 2 === 0 && 'no-highlight')} key={index}>{stack}</span>))
+                        content.descriptions.map((descItem, index) => getDescContent(descItem, index))
                       }
                     </div>
                   </div>
 
-                  <div className='resume-content-line'>
-                  <span className='resume-content-label'>Things I've done</span>
+                  <div className='stacks-container mt-40'>
+                    {
+                      content.stacks.map((stack, index) => (
+                        <span className={cn('stack-tag', index % 2 === 0 && 'no-highlight')} key={index}>{stack}</span>
+                      ))
+                    }
                   </div>
                 </>
               : <div className='no-content-to-show'>
