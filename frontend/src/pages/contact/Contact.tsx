e91bda167e006dbafcc6691d045c565fd25749b1
SebinSong <songxevin@gmail.com>
2024-06-16 12:50:19 +1200

    add a reciept confirmation screen to /contact

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -1,10 +1,12 @@
 // child components
 import { FormEvent, useState } from "react"
 import { useImmer } from "use-immer"
+import { useNavigate } from 'react-router-dom'
 import PageTemplate from "../PageTemplate"
 import ContactPageAnimation from "./contact-page-animation/ContactPageAnimation"
 import FormErrorMessage from "~/components/FormErrorMessage"
 import LoaderAnimation from '~/components/loader-animation/LoaderAnimation'
+import RocketIcon from '~/components/svg-icons/RocketIcon'
 
 // utils
 import {
@@ -23,6 +25,7 @@ type submitStatus = 'idle' | 'submitting' | 'success' | 'error'
 // helpers
 
 export default function Contact () {
+  const navigate = useNavigate()
   // local-state
   const [submitStatus, setSubmitStatus] = useState<submitStatus>('idle')
   const [form, updateForm] = useImmer<Inquiry>({
@@ -81,6 +84,10 @@ export default function Contact () {
     }
   }
 
+  const onHomeBtnClick = () => {
+    navigate('/')
+  }
+
   const updateFactory = (key: FormKeyUnion) => {
     return (e: any) => {
       const val = e.target.value
@@ -100,66 +107,94 @@ export default function Contact () {
       <ContactPageAnimation />
 
       <div className='page-content-wrapper'>
-        <div className='page-title-container'>
-          <h1 className='page-title'>
-            <span className='text'>Message me!</span>
-            <span className='circle-deco'></span>
-          </h1>
+        {
+          submitStatus === 'success'
+            ? <>
+                <div className='page-title-container is-submit-success'>
+                  <h1 className='page-title'>
+                    <span className='text'>Thanks!</span>
+                    <span className='circle-deco'></span>
+                  </h1>
+                </div>
 
-          <p className='page-desc'>If you would like to talk about working with me or hiring me, please leave your enquiry below.</p>
-        </div>
+                <div className='submit-success-container'>
+                  <RocketIcon classes='rocket-icon' width={120} />
 
-        <form onSubmit={submitHandler}>
-          <div className='sender-details'>
-            <div className='form-field'>
-              <label className='label-common'>Name:</label>
-              <input className={cn('input', isErrorActive('name') && 'is-error')}
-                type='text' placeholder='Your name'
-                onInput={updateFactory('name')}
-                data-vkey='name' />
+                  <p>I appreciate your interest. A confirmation e-mail will be sent to you shortly and I will respond to your message soon.</p>
+                  <p>I look forward to talking to you :)</p>
 
-              <FormErrorMessage formKey='name' formError={formError} />
-            </div>
+                  <button className='is-custom-border go-home-btn'
+                    onClick={onHomeBtnClick}>
+                    <span className='text'>
+                      <i className='icon-home is-prefix'></i>
+                      To home
+                    </span>
+                  </button>
+                </div>
+              </>
+            : <>
+                <div className='page-title-container'>
+                  <h1 className='page-title'>
+                    <span className='text'>Message me!</span>
+                    <span className='circle-deco'></span>
+                  </h1>
 
-            <div className='form-field mail-field'>
-              <label className='label-common'>E-mail:</label>
-              <input className={cn('input', isErrorActive('email') && 'is-error')}
-                type='text' placeholder='Your e-mail address'
-                onInput={updateFactory('email')} data-vkey='email' />
-              
-              <FormErrorMessage formKey='email' formError={formError} />
-            </div>
-          </div>
+                  <p className='page-desc'>If you would like to talk about working with me or hiring me, please leave your enquiry below.</p>
+                </div>
 
-          <div className='form-field'>
-            <label className='label-common'>
-              Title:
-              <span className='optional'>(Optional)</span>
-            </label>
-            <input className='input' type='text' placeholder='Enter title'
-              onInput={updateFactory('title')} data-vkey='title' />
-          </div>
+                <form onSubmit={submitHandler}>
+                  <div className='sender-details'>
+                    <div className='form-field'>
+                      <label className='label-common'>Name:</label>
+                      <input className={cn('input', isErrorActive('name') && 'is-error')}
+                        type='text' placeholder='Your name'
+                        onInput={updateFactory('name')}
+                        data-vkey='name' />
 
-          <div className='form-field'>
-            <label className='label-common'>Message:</label>
-            <textarea className={cn('textarea', isErrorActive('content') && 'is-error')}
-              placeholder='Message content'
-              onInput={updateFactory('content')} data-vkey='content' />
-            {
-              !isErrorActive('content') &&
-              <p className='helper'>Enter minimum 15 characters.</p>
-            }
+                      <FormErrorMessage formKey='name' formError={formError} />
+                    </div>
 
-            <FormErrorMessage formKey='content' formError={formError} />
-          </div>
+                    <div className='form-field mail-field'>
+                      <label className='label-common'>E-mail:</label>
+                      <input className={cn('input', isErrorActive('email') && 'is-error')}
+                        type='text' placeholder='Your e-mail address'
+                        onInput={updateFactory('email')} data-vkey='email' />
+                      
+                      <FormErrorMessage formKey='email' formError={formError} />
+                    </div>
+                  </div>
 
-          <div className='button-container'>
-            <button className='is-custom-border send-btn' type='submit' disabled={isSubmitting}>
-              <LoaderAnimation show={isSubmitting} />
-              <span className='text'>Send</span>
-            </button>
-          </div>
-        </form>
+                  <div className='form-field'>
+                    <label className='label-common'>
+                      Title:
+                      <span className='optional'>(Optional)</span>
+                    </label>
+                    <input className='input' type='text' placeholder='Enter title'
+                      onInput={updateFactory('title')} data-vkey='title' />
+                  </div>
+
+                  <div className='form-field'>
+                    <label className='label-common'>Message:</label>
+                    <textarea className={cn('textarea', isErrorActive('content') && 'is-error')}
+                      placeholder='Message content'
+                      onInput={updateFactory('content')} data-vkey='content' />
+                    {
+                      !isErrorActive('content') &&
+                      <p className='helper'>Enter minimum 15 characters.</p>
+                    }
+
+                    <FormErrorMessage formKey='content' formError={formError} />
+                  </div>
+
+                  <div className='button-container'>
+                    <button className='is-custom-border send-btn' type='submit' disabled={isSubmitting}>
+                      <LoaderAnimation show={isSubmitting} />
+                      <span className='text'>Send</span>
+                    </button>
+                  </div>
+                </form>
+              </>
+        }
       </div>
     </PageTemplate>
   )
